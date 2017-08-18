5ed422602f296a6a1563c01833432c17978e4dd3
Osamu Nakanishi <s15008@std.it-college.ac.jp>
2017-08-18 21:02:12 +0900

    修正 インポート抜けを修正

--- twitter/views.py
+++ twitter/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, render_to_response
+from django.shortcuts import render, render_to_response, reverse
 from django.template import RequestContext
 from django.contrib.auth.decorators import login_required
 from django.http import Http404, HttpResponseRedirect
